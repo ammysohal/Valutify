d00cd065f1b96d8a9d30316f58389372ce7671ec
parminderkhaira <parminderkhaira44@gmail.com>
2025-10-05 06:48:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5599196302-bf226",
+  "appId": "1:198447359643:web:ac1d8dba1a021587a7483e",
+  "apiKey": "AIzaSyAbRSCFDBavBPFK5ZvcXAwYRgP-QDtrIfA",
+  "authDomain": "studio-5599196302-bf226.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "198447359643"
+};
